8f7879f84360e71a954562e7ccfae181725a979f
cmis <cmis@cmis-hs06.(none)>
2016-01-29 10:56:23 +0700

    python

---
+++ hello.py
@@ -0,0 +1,10 @@
+myName = "Naja"
+myAge = float(15.4)
+myHeight = float(1.58)
+lengthSquare = int(3)
+lengthRectangle = int(8)
+heightRectangle = int(4)
+monthAge = myAge * 12
+yearsToLive = 80 - myAge
+heightInFeet = float(5.2)  
+
