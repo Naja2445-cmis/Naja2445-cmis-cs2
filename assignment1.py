808035b53a854d160aa034df2e3617f9b52a8ada
cmis <cmis@cmis-hs06.(none)>
2016-02-03 08:39:44 +0700

    assignment 1

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+myName = "Naja"
+myAge = float(15.4)
+myHeight = float(1.58)
+lengthSquare = int(3)
+lengthRectangle = int(8)
+heightRectangle = int(4)
+monthAge = myAge * 12
+yearsToLive = 80 - myAge
+heightInFeet = myHeight * 3.28084   
+averageHeight = myHeight - 157.3 
+areaSquare = lengthSquare * lengthSquare
+halfVolumeCube = (lengthSquare * lengthSquare * lengthSquare)/2 
+AreaOfRec = lengthRectangle * heightRectangle /9
+print "hello, I'm " + str(myName) + " and I am " + str(myAge) + " years old. My height right now is " + str(myHeight) + " in meters, and " + str(heightInFeet) + " in feet. I've got " + str(yearsToLive) + " more years to live."  
+print "hello, I'm ",myName, " and I am ",myAge, " years old. So I have about " , yearsToLive , " years to live. My current height is " , myHeight , ". " 
+
+
+
