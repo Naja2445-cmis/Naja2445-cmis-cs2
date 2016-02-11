8ee5be32befd038ca6b303cb7abd2491a86c2b35
cmis <cmis@cmis-hs06.(none)>
2016-02-11 13:35:22 +0700

    egfy

--- functions.py
+++ functions.py
@@ -1,6 +1,48 @@
-def add(a, b)
+import math 
+def add(a, b):
 	return (a + b)
 add(3, 4)
 
-def sub(a + b)
-	return (a + b) 
+
+def sub(a, b):
+	return (a - b) 
+sub(5, 3)
+
+
+def mul(a, b):
+	return (a * b)
+mul(4, 4)
+
+
+def div(a, b):
+	return (a / b)
+div(2, 3)
+
+
+def hours_from_second(a):
+	return (a/3600)
+hours_from_second(86400)
+
+
+def circle_area(a):
+	return (math.pi*(a**2))
+circle_area(5)
+
+
+def sphere_volume(a):
+	return ((4/3)*(math.pi)*(a**3))	
+sphere_volume(5)
+
+
+def avg_volume(a, b):
+	c = float(a / 2)
+	d = float(b / 2)
+	return ((4/3)*math.pi*(c**3)) + ((4/3)*math.pi*(d**3)) /2
+avg_volume(10, 20)
+
+def area(a, b, c):
+	s = (a + 
+area(1, 2, 2.5)
+
+
+
