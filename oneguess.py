35fdd68d68d5406a280ef0a0af2ac6add81741f7
cmis <cmis@cmis-hs06.(none)>
2016-03-14 10:55:27 +0700

    game

---
+++ oneguess.py
@@ -0,0 +1,16 @@
+def randomNumber():
+	number = int(raw_input("What is the minimum number?"))
+	
+
+
+
+
+I'm think of a number from {} - {}. 
+
+
+def main():
+
+	minimumNumber = raw_input("What is the minimum number?")
+	maximumNumber = raw_input("What is the mazimum number?")
+	
+
