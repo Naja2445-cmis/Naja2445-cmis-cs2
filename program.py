0b702ba4d5671f38c930cf56e4dad568956fc354
cmis <cmis@cmis-hs22.(none)>
2016-03-21 10:56:22 +0700

    dfghj

--- program.py
+++ program.py
@@ -1,48 +1,14 @@
-import math
-def multiply(w):
-	w1 = w * 10
-	return w1
+def BMR(weight, height, age):
+	BMR =  66 + ( 13.7 * weight) + ( 5 * height) - ( 6.8 * age)
 
-def multiply(h):
-	h1 = h * 6.25
-	return h1
-
-def multiply(a):
-	a1 = a * 5 
-	return a1
-
-def add(w1, h1):
-	wh = w1 + h1
-	return wh
-
-def sub(wh, a1):
-	a2 = wh - a1
-	return a2
-
-def sub(a2):
-	BMR = a2 + 5 
-	return BMR
-
-def output(name, w1, h1, a1, wh, a2, BMR):
+def output(weight, height, age)"
 	return """
-Hello there, {}!
-Did you know you will need {} calories to maintain your weight per day
-{} + {} = {}
-""". format(name, w1, h1, a1, wh, a2, BMR)
 
 def main():
-	name = raw_input("what's your name?: ")
-	w = raw_input("What is your weight? (kg)")
-	h = raw_input("What is your height?(cm) ")
-	a = raw_input("What is your age?")
-	w1 = multiply(int(w))
-	h1 = multiply(int(h))
-	a1 = multiply(int(a))	
-	wh = add(int(w1), int(h1))
-	a2 = sub(int(wh), int(a1))
-	BMR = sub(int(a2))
-	print output(name, w1, h1, a1, wh, a2, BMR)
+	weight = raw_input("What is your weight? (kg)")
+	height = raw_input("What is your height?(cm) ")
+	age = raw_input("What is your age?")
+
+	print output(weight, height, age)
 
 main() 
-
-
