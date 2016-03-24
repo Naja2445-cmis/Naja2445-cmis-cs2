4518d73fc88233055baadcf01c0e0f92a217dd4c
cmis <cmis@cmis-hs22.(none)>
2016-03-24 08:40:24 +0700

    asygu

---
+++ conditionals.py
@@ -0,0 +1,38 @@
+# My game will be like choosing your motorcycle to race with another player. You can pick many different kinds motorcycle, each motorcycle will have it's maximum speed. You have to choose the one that you think can win with it. You also can choose your opponent's motorcycle for them. You can't be sure if you're going to win because your speed will have to be multipled with a random number that the computer will choose.
+
+import math
+import random
+
+def speed(motorcycle):
+
+	if  motorcycle == ""
+		topspeed = 
+
+	elif motorcycle == ""
+		topspeed = 
+
+	elif motorcycle == ""
+		topspeed = 
+
+	elif motorcycle == ""
+		topspeed = 
+
+	elif motorcycle == ""
+		topspeed = 284 km/h
+
+	elif motorcycle == ""
+		topspeed = 325 km/h
+
+	elif motorcycle == "Kawasaki Ninja H2"
+		topspeed = 340 km/h
+
+	elif motorcycle == "MTT Turbine Y2K"
+		topspeed = 365 km/h
+
+	elif motorcycle == "Suzuki Hasyabusa"
+		topspeed = 397 km/h
+
+	elif motorcycle == "Dodge Tomahawk"
+		topspeed = 560 km/h
+
+
