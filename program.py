16298f9f606cf6fc433c60402556560186a07ac9
cmis <cmis@cmis-hs22.(none)>
2016-03-28 10:24:50 +0700

    adsfg

--- program.py
+++ program.py
@@ -1,16 +1,16 @@
-def BMR(weight, height, age):
-	BMR = (66+( 13.7 * weight)+( 5 * height)-( 6.8 * age))
-
-def output(weight, height, age):
+def output(weight, height, age, BMR):
 	return """
+Your weight is {} kg.
+Your height is {} cm.
+Your age is {} years old.
 Your BMR (Basal metabolic rate) is {}. 
-""". format(weight, height, age)
+""". format(weight, height, age, BMR)
 
 def main():
-	weight = raw_input("What is your weight? (kg)")
-	height = raw_input("What is your height?(cm) ")
-	age = raw_input("How old are you?")
+    weight = raw_input("What is your weight? (kg)")
+    height = raw_input("What is your height?(cm) ")
+    age = raw_input("How old are you?")
+    BMR = 10 * int(weight) + float(6.25) * int(height) - 5 * int(age) + 5
+    print output(weight, height, age, BMR)
 
-	print output(weight, height, age)
-
-main() 
+main()
