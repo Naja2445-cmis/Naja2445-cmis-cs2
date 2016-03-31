ed53bdac1bdb1d4171fccf40512ff7806b54afa2
cmis <cmis@cmis-hs22.(none)>
2016-03-31 08:39:49 +0700

    test

---
+++ cs2quiz2.py
@@ -0,0 +1,70 @@
+#PART 1: Terminology
+#1) Give 3 examples of boolean expressions.
+#a)True
+#b)False
+#c)
+#
+#2) What does 'return' do?
+#when you type return it will spits out something. 
+#
+#
+#
+#3) What are 2 ways indentation is important in python code?
+#a)indentation is important because we use it to know if the function is the child of another one. like def and we indent the return.
+#b)
+#
+#
+
+#PART 2: Reading
+#Type the values for 9 of the 12 of the variables below.
+#
+#problem1_a) 
+#problem1_b) 
+#problem1_c)
+#problem1_d)
+#
+#problem2_a) True
+#problem2_b) True
+#problem2_c) False
+#problem2_d) False
+#
+#problem3_a) 0.03
+#problem3_b) 0.5 
+#problem3_c) 0.5
+#problem3_d) 0.5 
+#
+#problem4_a) 5
+#problem4_b) 2
+#problem4_c) 
+#problem4_d) 4.5
+#
+
+#PART 3: Programming
+#Write a script that asks the user to type in 3 different numbers.
+#If the user types 3 different numbers the script should then print out the
+#largest of the 3 numbers.
+#If they don't, it should print a message telling them they didn't follow 
+#the directions.
+#Be sure to use the program structure you've learned (main function, processing function, output function)
+
+import math
+
+def output(a, b, c)
+	if a > b and a > c
+	return a 
+		print The largest number is {}.
+def result (a, b, c)
+	if b > a and b > c
+	return b
+		print The largest number is {}.
+def result2(a, b, c)
+	if c > a and c > b
+	return c
+		print The largest number is {}.
+
+
+def main():
+	a = raw_input("Type 3 different numbers")
+	
+
+
