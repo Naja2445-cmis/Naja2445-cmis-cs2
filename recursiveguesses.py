87c73885d67552e5389de940e83df1e832a37be9
cmis <cmis@cmis-hs22.(none)>
2016-05-04 13:34:41 +0700

    not done

---
+++ recursiveguesses.py
@@ -0,0 +1,24 @@
+import random
+import math
+
+def roundone(target, guessnum):
+	if target > "" :
+		print "That's too low". format(target, guessnum)
+	elif target == "":
+		print "That's correct!!". format(target, guessnum)
+	else: 
+		print "That's too high". format(target, guessnum)
+roundone(target, guessnum)
+
+
+
+
+
+
+def main():
+	starting = raw_input("Round 1")
+	target = random.randint(0, 100)
+	guessnum = int(raw_input("Guess the number: "))
+	roundone(target, guessnum)
+main()	
+
