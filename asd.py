f2ab0905e77eabe5e951af58921a6fcf208a0e5c
cmis <cmis@cmis-hs22.(none)>
2016-05-10 10:55:46 +0700

    dkjth

---
+++ asd.py
@@ -0,0 +1,13 @@
+def f(v, p):
+	if v == "bananas":
+		return p+1
+	else:
+		return p
+
+def main():
+	p = 0
+ 	p = f("bananas", p)
+	print p
+
+main()
+	
--- conditionals.py
+++ conditionals.py
@@ -1,70 +1,73 @@
 #my game will be a quiz game,the game will ask you some common knowledge stuff, and if you get it all right, you will win the game.
 import random
+import math
 def output(points, questionone):
 	if questionone == "apple": 
-		return True
-		points += 1
+		return points + 1
 	else:
-		return False
+		return points
 
 
 def result(points, questiontwo):
 	if questiontwo == "Amazon": 
-		return True
-		points += 1
+		return points + 1
 	else:
-		return False
+		return points
 
 def output2(points, questionthree):
 	if questionthree == "Alaska": 
-		return True
-		points += 1
+		return points + 1
 	else:
-		return False
+		return points
 
-def output2(points, questionfour):
-	if questionthree == "Poseidon": 
-		return True
-		points += 1
+def output3(points, questionfour):
+	if questionfour == "Poseidon": 
+		return points + 1
 	else:
-		return False    
+		return points    
 
-def output2(points, questionfive):
-	if questionthree == "five": 
-		return True
-		points += 1
+def output4(points, questionfive):
+	if questionfive == "five": 
+		return points + 1
 	else:
-		return False
+		return points
 
-def output2(points, questionsix):
-	if questionthree == "China": 
-		return True
-		points += 1
+def output5(points, questionsix):
+	if questionsix == "China": 
+		return points + 1
 	else:
-		return False
+		return points
 
-def output2(points, questionseven):
-	if questionthree == "Mona Lisa": 
-		return True
-		points += 1
+def output6(points, questionseven):
+	if questionseven == "Mona Lisa": 
+		return points + 1
 	else:
-		return False
+		return points
 
-def total(points):
-	total = points*random.random()
+def total(points):	
+	total = points * random.randint(0, 10)
+	return total
 	print total
-	
 
 
 
 def main():
 	points = 0
+	Intro = raw_input("The amount of questions you got right will be multipled by a random nuber(1-10), and that will be your score. If your score reach 50, then you pass!")
 	questionone = str(raw_input("What falling object is said to have Isaac Newton's theories about gravity?"))
+	output(points, questionone)
 	questiontwo = str(raw_input("Where in the world does the largest tropical rainforest grow?"))
+	result(points, questiontwo)
 	questionthree = str(raw_input("Which American state is nearest to the former Soviet Union?"))
+	output2(points, questionthree)
 	questionfour = str(raw_input("In Roman mythology, Neptune is the quivalent to which Greek god?"))
+	output3=(points, questionfour)
 	questionfive = str(raw_input("How many rings are on the Olympics flag?"))
+	output4(points, questionfive)
 	questionsix = str(raw_input("What country contains the largest number of people?"))
+	output5(points, questionsix)
 	questionseven = str(raw_input("What is the name of Leonardo Da Vinvi's most famous work?"))
-	total(points)
+	output6(points, questionseven)
+	total = points * random.randint(0, 10)
+	print total
 main()
