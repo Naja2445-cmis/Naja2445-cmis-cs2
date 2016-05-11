8f755912f83c6c1837d8f6e5665b4dbac6e0e445
cmis <cmis@cmis-hs14.(none)>
2016-05-11 13:35:13 +0700

    usg

--- conditionals.py
+++ conditionals.py
@@ -2,72 +2,72 @@
 import random
 import math
 def output(points, questionone):
-	if questionone == "apple": 
+	if questionone == "apple":
 		return points + 1
 	else:
 		return points
 
 
 def result(points, questiontwo):
-	if questiontwo == "Amazon": 
+	if questiontwo == "amazon": 
 		return points + 1
 	else:
 		return points
 
 def output2(points, questionthree):
-	if questionthree == "Alaska": 
+	if questionthree == "alaska": 
 		return points + 1
 	else:
 		return points
 
 def output3(points, questionfour):
-	if questionfour == "Poseidon": 
+	if questionfour == "poseidon": 
 		return points + 1
 	else:
 		return points    
 
 def output4(points, questionfive):
-	if questionfive == "five": 
+	if questionfive == "5": 
 		return points + 1
 	else:
 		return points
 
 def output5(points, questionsix):
-	if questionsix == "China": 
+	if questionsix == "china": 
 		return points + 1
 	else:
 		return points
 
 def output6(points, questionseven):
-	if questionseven == "Mona Lisa": 
+	if questionseven == "mona lisa": 
 		return points + 1
 	else:
 		return points
 
-def total(points):	
-	total = points * random.randint(0, 10)
-	return total
-	print total
+def total(points=0):	
+	points = points * random.randint(0, 10)
+	return points
+        print points
 
 
 
 def main():
-	points = 0
-	Intro = raw_input("The amount of questions you got right will be multipled by a random nuber(1-10), and that will be your score. If your score reach 50, then you pass!")
-	questionone = str(raw_input("What falling object is said to have Isaac Newton's theories about gravity?"))
-	output(points, questionone)
-	questiontwo = str(raw_input("Where in the world does the largest tropical rainforest grow?"))
-	result(points, questiontwo)
-	questionthree = str(raw_input("Which American state is nearest to the former Soviet Union?"))
-	output2(points, questionthree)
-	questionfour = str(raw_input("In Roman mythology, Neptune is the quivalent to which Greek god?"))
-	output3=(points, questionfour)
-	questionfive = str(raw_input("How many rings are on the Olympics flag?"))
-	output4(points, questionfive)
-	questionsix = str(raw_input("What country contains the largest number of people?"))
-	output5(points, questionsix)
-	questionseven = str(raw_input("What is the name of Leonardo Da Vinvi's most famous work?"))
-	output6(points, questionseven)
-	total = points * random.randint(0, 10)
-	print total
+    points = total(points=0)
+    Intro = raw_input("The amount of questions you got right will be multipled by a random nuber(1-10), and that will be your score. If your score reach 50, then you pass!")
+questionone = str(raw_input("What falling object is said to have Isaac Newton's theories about gravity?"))
+output(points, questionone)
+questiontwo = str(raw_input("Where in the world does the largest tropical rainforest grow?"))
+result(points, questiontwo)
+questionthree = str(raw_input("Which American state is nearest to the former Soviet Union?"))
+output2(points, questionthree)
+questionfour = str(raw_input("In Roman mythology, Neptune is the quivalent to which Greek god?"))
+output3=(points, questionfour)
+questionfive = str(raw_input("How many rings are on the Olympics flag?"))
+output4(points, questionfive)
+questionsix = str(raw_input("What country contains the largest number of people?"))
+output5(points, questionsix)
+questionseven = str(raw_input("What is the name of Leonardo Da Vinvi's most famous work?"))
+output6(points, questionseven)
+
+print points
 main()
