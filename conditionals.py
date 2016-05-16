29a7e05ea987188274058c43e1d05b01fcfa19a4
cmis <cmis@cmis-hs22.(none)>
2016-05-16 10:56:01 +0700

    dgt

--- conditionals.py
+++ conditionals.py
@@ -2,7 +2,7 @@
 import random
 import math
 def main():
-	
+	hey = raw_input("Hey!")
 	Name = raw_input("""
 Let's see if you will pass this common knowledge test?! Good luck!
 What's your name? """)
@@ -15,9 +15,16 @@ What's your name? """)
 	x= output(Name,passfail)
 	print x
 	
-
+def start(hey):
+	if hey == "Hey!":
+		print "Hey!"
+def intro(Name):
+	if Name == "Let's see if you will pass this common knowledge test?! Good luck! What's your name?":
+		print """Let's see if you will pass this common knowledge test?! Good luck!
+What's your name?"""
+		
 def question1():
-	question1= raw_input("""
+	questionone = raw_input("""
 What falling object is said to have Isaac Newton's theories about gravity?
 a. pineapple
 b. mango
@@ -25,17 +32,16 @@ c. apple
 d. watermelon
 (type a,b,c,d):
 		""")
-	if question1 == "a" or "b":
-		return 0
-	elif question1 == "d":
-		return 0
-	elif question1 == "c":
-		return random.random()
-	else: 
-		return 0
+	if questionone == "a" or "b" 0r "d": 
+			return 0
+	elif questionone == "c":
+			return random.random()
+	else:
+		print "please type in a, b, c, or d"
+		question1()
 
 def question2():
-	question2= raw_input("""
+	questiontwo= raw_input("""
 Where in the world does the largest tropical rainforest grow?
 a. amazon
 b. pacific temperate rainforest
@@ -43,17 +49,17 @@ c. santa elena cloud forest reserve
 d. tongass national forest
 (type a,b,c,d):
 		""")
-	if question2 == "c" and "b":
-		return 0
-	elif question2 == "d":
-		return 0
-	elif question2 == "a":
-		return random.random()
-	else: 
-		return 0
-
+	if questiontwo != "a" or "b" or "c" or "d":
+		print "please type in a, b, c, or d"
+		question2()
+	else:
+		if questionetwo == "a":
+			return random.random()
+		else: 
+			return 0
+		
 def question3():
-	question3= raw_input("""
+	questionthree= raw_input("""
 Which American state is nearest to the former Soviet Union?
 a. Washington D.C
 b. Oregon
@@ -61,17 +67,17 @@ c. Maine
 d. Alaska
 (type a,b,c,d):
 		""")
-	if not question3 == "c" or question3== "b": 
-		return 0
-	elif question3 == "a":
-		return 0
-	elif question3 == "d":
-		return random.randint(0,2)
-	else: 
-		return 0
+	if questionthree != "a" or "b" or "c" or "d":
+		print "please type in a, b, c, or d"
+		question3()
+	else:
+		if questionthree == "d":
+			return random.randint(0,2)
+		else: 
+			return 0
 
 def question4():
-	question4= raw_input("""
+	questionfour= raw_input("""
 In Roman mythology, Neptune is the quivalent to which Greek god?"
 a. Zeus
 b. Poseidon
@@ -79,15 +85,18 @@ c. Percy Jackson
 d. Hades
 (type a,b,c,d):
 		""")
-	if question4 == "c" or "b" or "a":
-		return 0
-	elif question4 == "d":
-		return random.random()
-	else: 
-		return 0
+	if questionfour != "a" or "b" or "c" or "d":
+		print "please type in a, b, c, or d"
+		question4()
+	else:
+		if questionfour == "b":
+			return random.random()
+		else: 
+			return 0
+		
 
 def question5():
-	question5 = raw_input("""
+	questionfive = raw_input("""
 How many rings are on the Olympics flag?
 a. 6
 b. 4
@@ -95,15 +104,18 @@ c. 7
 d. 5
 (type a, b, c, d):
 		""")
-	if question5 == "a" or "b" or "c":
-		return 0
-	elif question5 == "d":
-		return random.random()
+	if questionfive != "a" or "b" or "c" or "d":
+		print "please type in a, b, c, or d"
+		question5()
 	else:
-		return random.random()
-
+		if questionfive == "c":
+			return random.random()
+		else: 
+			return 0
+		
+		
 def question6():
-	question6 = raw_input("""
+	questionsix = raw_input("""
 What country contains the largest number of people?
 a. Russia
 b. China
@@ -111,13 +123,17 @@ c. India
 d. United States
 (type a, b, c, d):
 		""")
-	if question6 != "b":
-		return 0
-	elif question6 == "b":
-		return random.randint(1,3)
+	if questionsix != "a" or "b" or "c" or "d":
+		print "please type in a, b, c, or d"
+		question6()
+	else:
+		if questionsix == "c":
+			return random.randint(1, 3)
+		else: 
+			return 0
 
 def question7():
-	question7 = raw_input("""
+	questionseven = raw_input("""
 What is the name of Leonardo Da Vinci's most famous work?
 a. Mona Lisa
 b. The Last Supper
@@ -125,13 +141,16 @@ c. Vitruvian Man
 d. Virgin of the Rocks
 (type a, b, c, d):
 		""")
-	if question7 == "b" or "c" or "d":
-		return 0
-	elif question7 == "a":
-		return random.random()
-	else:
-		return random.random()
 
+	if questionseven != "a" or "b" or "c" or "d":
+		print "please type in a, b, c, or d"
+		question7()
+	else:
+		if questionseven == "c":
+			return random.random()
+		else: 
+			return 0
+		
 
 def calculate(questionA,questionB,questionC,questionD,questionE,questionF, questionG):
 	return questionA,questionB,questionC,questionD,questionE,questionF, questionG
@@ -145,7 +164,7 @@ def point():
 	questionF= question6()
 	questionG= question7()
 	calculation= calculate(questionA,questionB,questionC,questionD,questionE,questionF, questionG)
-	if calculation >= 1:
+	if calculation >= 0.5:
 		return True
 	else:
 		return False
