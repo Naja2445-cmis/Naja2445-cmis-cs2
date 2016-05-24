f6d009b465fb4874953247c576e5270a2871be6b
cmis <cmis@cmis-hs02.(none)>
2016-05-24 13:33:58 +0700

    lol

---
+++ loop.py
@@ -0,0 +1,23 @@
+#while True:
+#	print ":)"
+
+#while False:
+#	print ":)"
+
+#x = 10
+#while x >= 0:
+#	print x  
+#	x -= 1
+
+def countdown(x):
+	while x > 0:
+		print x
+		x -= 1
+
+def countup(x):
+	while x < 11:
+		print x
+		x += 1
+
+countdown(10)
+countup(0)
