a93962f59be825996da6a1992d523a6d36bd0200
cmis <cmis@cmis-hs22.(none)>
2016-05-26 08:42:58 +0700

    dg

--- loop.py
+++ loop.py
@@ -9,15 +9,34 @@
 #	print x  
 #	x -= 1
 
-def countdown(x):
-	while x > 0:
-		print x
-		x -= 1
+#def countdown(x):
+#	while x > 0:
+#		print x
+#		x -= 1
 
-def countup(x):
-	while x < 11:
-		print x
-		x += 1
+#def countup(x):
+#	while x < 11:
+#		print x
+#		x += 1
 
-countdown(10)
-countup(0)
+#def countxandy(x, y):
+#	while x <= y:
+#		print x
+#		x += 1
+#	while x >= y:
+#		print x
+#		x-= y
+
+def addodd(n):
+	if n % 2 != 0:
+		while n >= 0: 
+			return n 
+			n += 1
+	
+
+
+#countxandy(5, 10)
+#countxandy(10, 5)
+
+#countdown(10)
+#countup(0)
